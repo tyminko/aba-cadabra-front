1e1cdbfdf906e2de965d355909d8096422629fa5
maxim <tyminko@gmail.com>
2020-08-21 13:05:26 +0200

    + update ProfileCell (add avatar)

--- src/views/components/ProfileCell.vue
+++ src/views/components/ProfileCell.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="profile-cell">
-    <router-link :to="{name: 'profile', params: {id: this.profile.id}}"><h2>{{displayName}}</h2>
-      <div class="desc">{{positionStr}}</div>
+    <router-link :to="{name: 'profile', params: {id: this.profile.id}}">
+      <header class="flex">
+        <div class="avatar flex-shrink-0 flex-grow-0 w-base h-base rounded-full overflow-hidden bg-gray-200 mr-sm">
+          <img
+            v-if="thumbnailUrl"
+            :src="thumbnailUrl"
+            class="w-full h-full object-cover"
+            alt="team member portrait">
+        </div>
+        <div class="person-info flex-grow">
+          <h2>{{displayName}}</h2>
+          <div class="desc">{{positionStr}}</div>
+        </div>
+      </header>
       <!--    <img v-if="thumbnailUrl" :src="thumbnailUrl" class="mb-sm">-->
       <div class="excerpt mt-sm text-sm">{{description}}...</div>
     </router-link>
@@ -42,17 +54,20 @@ export default {
       if (!this.profile || !this.profile.attachments) return
       const post = this.profile
       let src = null
-      const firstVisualAttachment = Object.values(post.attachments).find(attachment => {
-        return (attachment.type || '').startsWith('image/') ||
+      const firstVisualAttachment = Object.values(post.attachments)
+        .sort((a, b) => a.order - b.order)
+        .find(attachment => {
+          return (attachment.type || '').startsWith('image/') ||
           attachment.srcSet.hasOwnProperty('full') ||
           attachment.srcSet.hasOwnProperty('preview')
-      })
+        })
       if (firstVisualAttachment) {
         src = firstVisualAttachment.srcSet
       }
       if (!src) return ''
+      const originalIsImage = (firstVisualAttachment.type || '').startsWith('image/')
       const { full, preview, original } = src
-      return (preview || full || original || {}).url
+      return (preview || full || (originalIsImage ? original : null) || {}).url
     },
 
     description () {
@@ -69,6 +84,8 @@ export default {
   .profile-cell {
     .excerpt {
       @include multi-line-truncate(3);
+    }
+    .avatar {
     }
   }
 </style>
