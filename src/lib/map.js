51488ad7cb7480899f99972beae6caf2ebc1738b
pxflux <mail@aleksandrkomarov.com>
2025-04-25 13:53:01 +0200

    back to original file maps

--- src/lib/map.js
+++ src/lib/map.js
@@ -1,2 +1,18 @@
 export NODE_OPTIONS=--openssl-legacy-provider
 npm run build
+
+// From types.d.ts
+interface Location {
+  address: string,
+  city: string,
+  country: string,
+  countryCode: string,
+  lat: number,
+  lng: number
+}
+
+mapOptions: {
+  disableDefaultUI: true,
+  backgroundColor: '#fff',
+  styles: [...abaMapStyle.basic]
+}
